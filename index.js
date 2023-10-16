e4fdaf6768e6778a9c2c6f584b2af07fb76d3342
nicolecraven <craventhegreat@gmail.com>
2023-10-16 18:28:09 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+// const customerName = "bob";
+//let customerName = "bob";
+// console.log(customerName);
+var customerName = "bob";
+console.log(customerName);
+
+var customerName = "bob";
+
+ function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    
+ }
+
+ function setBestCustomer() {
+    window.bestCustomer = "not bob";
+    
+ }
+ function overwriteBestCustomer(bestCustomer) {
+    window.bestCustomer = bestCustomer;
+    
+ }
+ const leastFavoriteCustomer = 10;
+ function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 20;
+    
+ }
+
+
